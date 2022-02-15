413aec4b2148784c3d551d5c9d5a0aada99db36e
Chungsang Tom Lam <erinata@gmail.com>
2022-02-16 01:12:49 +0800

    better return values

--- kfold_template.py
+++ kfold_template.py
@@ -10,6 +10,10 @@ def run_kfold(data, target, split_number, machine, use_accuracy=0, use_confusion
 	kfold_object = KFold(n_splits=split_number)
 	kfold_object.get_n_splits(data)
 
+	results_r2 = []
+	results_accuracy = []
+	results_confusion = []
+
 	for training_index, test_index in kfold_object.split(data):
 		data_training = data[training_index]
 		target_training = target[training_index]
@@ -18,12 +22,13 @@ def run_kfold(data, target, split_number, machine, use_accuracy=0, use_confusion
 		# machine = linear_model.LinearRegression()
 		machine.fit(data_training, target_training)	
 		new_target = machine.predict(data_test)
-		print(metrics.r2_score(target_test, new_target))
+		results_r2.append(metrics.r2_score(target_test, new_target))
 		if use_accuracy == 1:
-			print(metrics.accuracy_score(target_test, new_target))
+			results_accuracy.append(metrics.accuracy_score(target_test, new_target))
 		if use_confusion == 1:
-			print(metrics.confusion_matrix(target_test, new_target))
-
+			results_confusion.append(metrics.confusion_matrix(target_test, new_target))
+	return results_r2, results_accuracy, results_confusion
+	
 
 if __name__ == '__main__':
 	dataset = pandas.read_csv("dataset.csv")
--- use_kfold_with_template.py
+++ use_kfold_with_template.py
@@ -5,6 +5,18 @@ from sklearn import linear_model
 dataset = pandas.read_csv("dataset.csv")
 target = dataset.iloc[:,2].values
 data = dataset.iloc[:,3:9].values
-# kfold_template.run_kfold(data, target, 4, linear_model.LinearRegression())
-kfold_template.run_kfold(data, target, 4, linear_model.LogisticRegression(), 1, 1)
+# r2_scores = kfold_template.run_kfold(data, target, 4, linear_model.LinearRegression())
+# print(r2_scores)
+
+r2_scores, accuracy_scores, confusion_matrices = kfold_template.run_kfold(data, target, 4, linear_model.LogisticRegression(), 1, 1)
+
+print(r2_scores)
+
+print(accuracy_scores)
+
+for confusion_matrix in confusion_matrices:
+	print(confusion_matrix)
+
+
+
 
