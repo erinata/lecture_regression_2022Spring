c42d3e805edee857fb89b0064acd333e9b1424cd
Chungsang Tom Lam <erinata@gmail.com>
2022-02-18 01:08:33 +0800

    high order polynominal

--- kfold_template.py
+++ kfold_template.py
@@ -21,6 +21,7 @@ def run_kfold(data, target, split_number, machine, use_accuracy=0, use_confusion
 		target_test = target[test_index]
 		# machine = linear_model.LinearRegression()
 		machine.fit(data_training, target_training)	
+		# print(machine.score(data_training, target_training))
 		new_target = machine.predict(data_test)
 		results_r2.append(metrics.r2_score(target_test, new_target))
 		if use_accuracy == 1:
@@ -28,7 +29,7 @@ def run_kfold(data, target, split_number, machine, use_accuracy=0, use_confusion
 		if use_confusion == 1:
 			results_confusion.append(metrics.confusion_matrix(target_test, new_target))
 	return results_r2, results_accuracy, results_confusion
-	
+
 
 if __name__ == '__main__':
 	dataset = pandas.read_csv("dataset.csv")
--- use_kfold_with_template.py
+++ use_kfold_with_template.py
@@ -3,19 +3,21 @@ import pandas
 from sklearn import linear_model
 
 dataset = pandas.read_csv("dataset.csv")
-target = dataset.iloc[:,2].values
+
+target = dataset.iloc[:,0].values
 data = dataset.iloc[:,3:9].values
-# r2_scores = kfold_template.run_kfold(data, target, 4, linear_model.LinearRegression())
-# print(r2_scores)
 
-r2_scores, accuracy_scores, confusion_matrices = kfold_template.run_kfold(data, target, 4, linear_model.LogisticRegression(), 1, 1)
-
+r2_scores = kfold_template.run_kfold(data, target, 4, linear_model.LinearRegression())
 print(r2_scores)
 
-print(accuracy_scores)
+# r2_scores, accuracy_scores, confusion_matrices = kfold_template.run_kfold(data, target, 4, linear_model.LogisticRegression(), 1, 1)
 
-for confusion_matrix in confusion_matrices:
-	print(confusion_matrix)
+# print(r2_scores)
+
+# print(accuracy_scores)
+
+# for confusion_matrix in confusion_matrices:
+# 	print(confusion_matrix)
 
 
 
